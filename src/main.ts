b2ae2bf560b1228fd3a36dcf638360532b10fd23
src0730 <davemark0730@hotmail.com>
2025-04-14 23:11:51 +0800

    feat: 使用 createCustomElement 註冊 Super Grid Web Component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
+import { createApplication } from '@angular/platform-browser';
+import { SuperGridWidgetComponent } from '@components';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+(async () => {
+  const app = await createApplication();
+  const superGridWidgetElement = createCustomElement(SuperGridWidgetComponent, {
+    injector: app.injector,
+  });
+  customElements.define('super-grid', superGridWidgetElement);
+
+  console.log('Super Grid Web Component registered successfully!');
+})();
